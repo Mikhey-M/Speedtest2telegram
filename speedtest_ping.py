ace9915cdd9a20a8f85a37e2343c74259255c7be
Mikhey-M <71590807+Mikhey-M@users.noreply.github.com>
2020-09-28 14:44:01 +0300

    Update speedtest_ping.py

--- speedtest_ping.py
+++ speedtest_ping.py
@@ -3,7 +3,7 @@ import requests
 from subprocess import PIPE, Popen
 
 #Name Lastname
-Name_LastName = str(input("Введите Имя и Фамилию: "))
+Name_LastName = str(input("Enter your first and last name: "))
 
 #ping
 res = Popen('ping -n 300 ya.ru | find "Пакетов" & ping -n 1 ya.ru | find "Минимальное"', shell=True, stdout=PIPE)
@@ -22,9 +22,9 @@ Speed_test = (f" \n Download speed: {download_mbs} Mb/s \n Upload Speed : {uploa
 ping_out = (f" \n Ping info: \n {out}")
 
 #telegram
-api_token = '1241957629:AAE18fewdITf4Zc-v46Grwp7k-P_lnq1pIc'
+api_token = 'token'
 
 requests.get('https://api.telegram.org/bot{}/sendMessage'.format(api_token), params=dict(
-   chat_id='-348050921',
+   chat_id='id',
    text=Name_LastName + Speed_test + ping_out
 ))
